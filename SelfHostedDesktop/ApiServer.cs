540781e4773eaa26eabe82a903b78545765db864
Steven Tharwat <stevven.helmy@gmail.com>
2025-05-22 19:59:18 +0300

    V1.0

---
+++ SelfHostedDesktop/ApiServer.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace SelfHostedDesktop
+{
+
+
+    public static class ApiServer
+    {
+        private static IHost? _host;
+
+        public static void Start()
+        {
+            if (_host != null) return;
+
+            _host = Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseKestrel()
+                              .UseUrls("http://localhost:5000")
+                              .Configure(app =>
+                              {
+                                  app.UseRouting();
+                                  app.UseEndpoints(endpoints =>
+                                  {
+                                      endpoints.MapGet("/hello", () => "Hello from WPF API!");
+                                  });
+                              });
+                })
+                .Build();
+
+            _host.Start();
+        }
+
+        public static async Task Stop()
+        {
+            if (_host != null)
+            {
+                await _host.StopAsync();
+                _host = null;
+            }
+        }
+    }
+
+}
---
+++ SelfHostedDesktop/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace SelfHostedDesktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartApi_Click(object sender, RoutedEventArgs e)
+        {
+            ApiServer.Start();
+        }
+
+        private async void StopApi_Click(object sender, RoutedEventArgs e)
+        {
+            await ApiServer.Stop();
+        }
+    }
+}
